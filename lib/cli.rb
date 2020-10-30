d19c0e40a13692496cfb31093e6a121659423677
Michael Sackstein <m_sackstein@yahoo.com>
2020-10-30 00:13:17 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,13 +1,14 @@
 class CLI
 
     def start
-        puts "\n"
+        puts ""
         puts "Welcome to the movie list app"
         API.fetch_movies 
         self.menu 
     end
 
     def menu 
+<<<<<<< HEAD
         puts "\n"
         puts "MAIN MENU - Please enter a selection number (0-4) from the list below to continue or type any other key to exit, then press enter/return!"
         puts "\n"
@@ -18,47 +19,69 @@ class CLI
         puts "To view a complete list of movies with additional options, press 0"
         puts "\n"
         user_input = gets.strip.downcase.to_i
+=======
+        puts ""
+        puts "---------------MAIN MENU--------------"
+        puts ""
+        puts "Please enter a selection number (1-5) from the list below to continue or type any other key to exit, then press enter/return!"
+        puts ""
+        puts "To view a complete list of movies, press 1"
+        puts "To view a complete list of movies in ascending alphabetical order, press 2"
+        puts "To view a complete list of movies in descending alphabetical order, press 3"
+        puts "To view a complete list of movies by rating in ascending order, press 4"
+        puts "To view a complete list of movies by rating in descending order, press 5"
+        puts ""
+        user_input = gets.strip.to_i
+>>>>>>> d13c6a4b9a3cc923f24a74aa3dd6917c95c137a7
         case user_input
-        when 0
+        when 1
             display_list_of_movies 
             ask_user_for_movie_choice
             sleep(2)
             menu 
-        when 1
+        when 2
             display_sorted_title_asc
             sleep(2)
             menu 
-        when 2
+        when 3
             display_sorted_title_desc
             sleep(2)
             menu 
-        when 3
+        when 4
             display_sorted_rt_score_asc
             sleep(2)
             menu 
-        when 4
+        when 5
             display_sorted_rt_score_desc
             sleep(2)
             menu 
         else
-            puts "Goodbye!"
+            puts ""
+            puts "Thank you, Goodbye!"
+            puts ""
         end  
     end
 
     def display_list_of_movies
-        puts "\n"
+        puts ""
+        puts "---------------COMPLETE MOVIE LIST--------------"
+        puts ""
         Movie.all.each.with_index(1) do |movie, index| 
             puts "#{index}. #{movie.title}"
         end
     end
 
     def ask_user_for_movie_choice
-        puts "\n"
+        puts ""
         puts "Enter the number of the movie you'd like to know more about, then press enter/return!"
+        puts ""
         index = gets.strip.to_i - 1
+        
+        
         until index.between?(0, Movie.all.length - 1)
-            puts "\n"
+            puts ""
             puts "Invalid entry! Please choose a valid number, then press enter/return!"
+            puts ""
             index = gets.strip.to_i - 1
         end
         movie_instance = Movie.all[index]
@@ -67,7 +90,7 @@ class CLI
 
     def display_movie_details(movie)
         sleep(1)
-        puts "\n"
+        puts ""
         puts "Title: " + movie.title
         puts "Director: " + movie.director
         puts "Producer: " + movie.producer
@@ -77,28 +100,36 @@ class CLI
     end
 
     def display_sorted_title_asc
-        puts "\n"
+        puts ""
+        puts "---------------MOVIE LIST WITH TITLES ASCENDING--------------"
+        puts ""
         Movie.sort_title_asc.each.with_index(1) do |movie, index| 
             puts "#{index}. #{movie.title}"
         end
     end
 
     def display_sorted_title_desc
-        puts "\n"
+        puts ""
+        puts "---------------MOVIE LIST WITH TITLES DESCENDING--------------"
+        puts ""
         Movie.sort_title_desc.each.with_index(1) do |movie, index| 
             puts "#{index}. #{movie.title}"
         end
     end
 
     def display_sorted_rt_score_asc
-        puts "\n"
+        puts ""
+        puts "---------------MOVIE LIST WITH SCORES ASCENDING--------------"
+        puts ""
         Movie.sort_rt_score_asc.each.with_index(1) do |movie, index| 
             puts "#{index}. #{movie.title} - " + movie.rt_score
         end
     end
 
     def display_sorted_rt_score_desc
-        puts "\n"
+        puts ""
+        puts "---------------MOVIE LIST WITH SCORES DESCENDING--------------"
+        puts ""
         Movie.sort_rt_score_desc.each.with_index(1) do |movie, index| 
             puts "#{index}. #{movie.title} - " + movie.rt_score
         end
